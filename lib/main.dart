f101744ed8dae04e4a889807c38df33a5c246e18
Reivaldy-Zaen <reivaldyz@gmail.com>
2025-11-18 11:43:05 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'login.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'zaen',
-      theme: ThemeData(
-      
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'tes',
+      home: Login(),
     );
   }
 }

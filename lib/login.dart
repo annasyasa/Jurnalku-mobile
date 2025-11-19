d83a811a212cd310f2e46e4b09323b97dc045d13
pannca <pancaprasetia@smkwikrama.sch.id>
2025-11-19 19:24:43 +0700

    feat: menambahkan page catatan sikap

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jurnalku_mobile/catatan_sikap.dart';
 import 'package:jurnalku_mobile/settings.dart';
 import 'package:jurnalku_mobile/panduan.dart';
 import 'package:jurnalku_mobile/permintaan_saksi.dart';
@@ -73,6 +74,17 @@ class Login extends StatelessWidget {
                       Navigator.push(
                         context,
                         MaterialPageRoute(builder: (_) => const PermintaanSaksi()),
+                      );
+                    });
+                  },
+                ),
+                PopupMenuItem(
+                  child: const Text("Catatan Sikap"),
+                  onTap: () {
+                    Future.delayed(Duration.zero, () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => const CatatanSikapPage()),
                       );
                     });
                   },
